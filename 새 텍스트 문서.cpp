8a0910aab6cd80e39602b11d1334b83c563ba17d
choihyun2020 <gusgh28@naver.com>
2021-04-22 17:00:56 +0900

    새 텍스트 문서

---
+++ 새 텍스트 문서.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+int main(void) {
+ 
+	int a1, a2;
+	int b1, b2, b3;
+	int c;
+ 
+	scanf("%d\n%d", &a1, &a2);
+ 
+	b1 = ((a1%100)%10);
+	b2 = ((a1%100)/10);
+	b3 = (a1/100);
+ 
+	c = a1 * a2;
+ 
+	printf("%d\n%d\n%d\n%d", b1, b2, b3, c);
+ 
+ 
+	return 0;
+}
