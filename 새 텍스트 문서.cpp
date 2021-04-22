6120eae8a109b87558ab3e25dc694471c76c22e9
choihyun2020 <gusgh28@naver.com>
2021-04-22 17:07:25 +0900

    새 텍스트 문서

--- 새 텍스트 문서.cpp
+++ 새 텍스트 문서.cpp
@@ -1,22 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
- 
+
 int main(void) {
- 
+
 	int a1, a2;
 	int b1, b2, b3;
 	int c;
- 
+
 	scanf("%d\n%d", &a1, &a2);
- 
-	b1 = ((a1%100)%10);
-	b2 = ((a1%100)/10);
-	b3 = (a1/100);
- 
+
+	b1 = a1*((a2 % 100) % 10);
+	b2 = a1*((a2 % 100) / 10);
+	b3 = a1*(a2 / 100);
+
 	c = a1 * a2;
- 
+
 	printf("%d\n%d\n%d\n%d", b1, b2, b3, c);
- 
- 
+
+
 	return 0;
 }
